ec2f33e9c243dc1fca9214c5d696136c0c64b9e7
tony t <antonios.takos51@student.qcc.cuny.edu>
2025-09-03 10:49:00 -0400

    update lab 2

--- lab2_takos/lab2_takos.py
+++ lab2_takos/lab2_takos.py
@@ -3,4 +3,5 @@ Antonios Takos
 Lab 2, Mastering Git Workflow
 September 3rd, 2025
 """
-print("Welcome to Git Commands")
+username = input("Enter a username:")
+print(f"Welcome to Git Commands {username}")
