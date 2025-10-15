5c631725b5635fd673eebf68360bc58bd77dafbe
tonyboithememer <163060193+tonyboithememer@users.noreply.github.com>
2025-10-15 15:54:43 +0000

    complete lab 10

---
+++ lab10_file_test/file_operations.py
@@ -0,0 +1,36 @@
+"""
+Antonios Takos
+Oct 15, 2025
+Lab 9: file operation test
+"""
+def write_file(filename, msg):
+    with open(filename, "w") as file:
+        file.write(msg)
+
+def read_file(filename):
+    with open(filename, "r") as file:
+        file.readlines()
+
+def append_file(filename):
+    with open(filename, "a") as file:
+        file.write("\nNew line added!")
+
+# function from exercise lab 7 (yesterday)
+"""
+create a function, named email_read(), that reads a txt file and returns the number of users that have @gmail, @yahoo, and @hotmail email address. The function should have try-exception statement. You can test the function using the file user_email.txt file.
+"""
+def email_read(filename, email):
+    count_email = 0
+    with open(filename, "r") as file1:
+        filelines = file1.readlines()
+        # loop through each item in 'filelines'
+        for eachline in filelines:
+            if email in eachline:
+                count_email += 1
+        return count_email
+
+def write_report(filename, count_gmail, count_yahoo, count_hotmail):
+    with open(filename, "w") as report:
+        report.write(f"gmail = {count_gmail}\n")
+        report.write(f"yahoo = {count_yahoo}\n")
+        report.write(f"hotmail = {count_hotmail}\n")
---
+++ lab10_file_test/test_file_operations.py
@@ -0,0 +1,74 @@
+"""
+Antonios Takos
+Oct 15, 2025
+lab 10, unittest for file operations
+"""
+import unittest
+import os
+from file_operations import read_file, write_file, append_file
+class TestFileOperations(unittest.TestCase):
+    def setUp(self):
+        # set up temporary test file name before each test.
+        self.filename = "test_file.txt"
+        self.msg = "Antonios Takos"
+    
+    def tearDown(self):
+        # remove the test file after each test
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
+
+    def test_write_file(self):
+        # test writing text to a file
+        msg = "Antonios Takos"
+        with open(self.filename, "w") as f:
+            f.write(msg)
+
+        # verify file exists and content matches
+        self.assertTrue(os.path.exists(self.filename))
+        with open(self.filename, "r") as f:
+            result = f.read()
+
+        self.assertEqual(result,msg)
+
+    def test_read_file(self):
+        # test reading text from a file
+        expected_content = "Read me!"
+        with open(self.filename, "w") as f:
+            f.write(expected_content)
+
+            with open(self.filename, "r") as f:
+                data = f.read()
+            
+            self.assertEqual(data,expected_content)
+
+        def test_append_file(self):
+            # test appending test to an existing file
+            inital_content = "Line one"
+            append_content = "\nLine two"
+
+            with open(self.filename, "w") as f:
+                f.write(inital_content)
+
+            with open(self.filename, "a") as f:
+                f.write(append_content)
+
+            with open(self.filename, "r") as f:
+                final_data = f.read()
+
+            self.assertEqual(final_data, inital_content + append_content)
+        
+    # LAB EXERCISE
+    def test_email_read(self):
+        expected_content = "Nick Carter, ncarter@yahoo.com\nVanessa Singh, vsingh@gmail.com\nAnn Parker, aparker@gmail.com\nPhil Chen, pchen@hotmail.com\nFiona Smith, fsmith@gmail.com\nJoshue Candela, jcandela@yahoo.com\nCarl Peterson, cpeterson@hotmail.com\nKevin Brook, kbrook@gmail.com \nTheresa Ng, tng@hotmail.com\nJoe Hunter, jhunter@yahoo.com\nPatricia Franco, pfranco@gmail.com\nMarie Mansion, mmansion@gmail.com\nCarla Lee, clee@hotmail.com"
+        with open(self.filename, "w") as f:
+            f.write(expected_content)
+
+        with open(self.filename, "r") as f:
+            data = f.read()
+            
+        self.assertEqual(data,expected_content)
+
+
+# run the unit tests automatically when the file is run
+if __name__ == "__main__":
+    unittest.main()
