9308858615762d5f76db4230e5afb0bb034c0a91
tonyboithememer <163060193+tonyboithememer@users.noreply.github.com>
2025-11-19 16:38:51 +0000

    to do list with Django

--- dolist/urls.py
+++ dolist/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     # load page of the app will be sent to the index.html
     path('', views.index, name = 'index'),
+    path('add', views.addTodoitem, name = 'add')
 ]
--- dolist/views.py
+++ dolist/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . models import Todolist
+from .forms import Todolistform
+from django.views.decorators.http import require_POST
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    todo_tasks = Todolist.objects.order_by('id')
+    form = Todolistform()
+    context = {'todo_items' : todo_tasks, 'form':form}
+    return render(request, 'index.html', context)
+
+@require_POST
+def addTodoitem(request):
+    form = Todolistform(request.POST)
+
+
+    # capture data from the form when the Add to list button is pressed
+    if form.is_valid:
+        form.save()
+
+
+    return redirect('index')
