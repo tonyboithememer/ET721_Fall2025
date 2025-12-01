be47bda5a3f93214c20772377a70727a29ba95ad
tonyboithememer <163060193+tonyboithememer@users.noreply.github.com>
2025-12-01 17:00:10 +0000

    complete lab15 stuff

--- lab17_unittest/dolist/urls.py
+++ lab17_unittest/dolist/urls.py
@@ -4,5 +4,14 @@ from . import views
 urlpatterns = [
     # load page of the app will be sent to the index.html
     path('', views.index, name = 'index'),
-    path('add', views.addTodoitem, name = 'add')
+    path('add', views.addTodoitem, name = 'add'),
+
+    # add a path of the completed items in a list to our database
+    path('completed/<todo_id>', views.completedTodo, name = 'completed'),
+
+    # delete tasks that are marked as completed
+    path('deletecompleted', views.deletecompleted, name = 'deletecompleted'),
+
+    # delete all tasks
+    path('deleteall', views.deleteAll, name = 'deleteall'),
 ]
